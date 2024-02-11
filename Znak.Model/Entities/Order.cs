88310a17ce8cacc510181cc9438c685c4873def8
Sanjar Akhmedov <sanjarakhmedov026@gmail.com>
2024-02-11 18:19:31 +0500

    changes

--- Znak.Model/Entities/Order.cs
+++ Znak.Model/Entities/Order.cs
@@ -2,8 +2,13 @@ namespace Znak.Model.Entities
 {
     public abstract class Order : BaseEntity
     {
+        public Order()
+        {
+            KM = Guid.NewGuid();
+        }
         public string Name { get; set; }
         public string Description { get; set; }
         public decimal Price { get; set; }
+        public Guid KM { get; set; }
     }
 }
--- Znak.Model/MockData/MockData.cs
+++ Znak.Model/MockData/MockData.cs
@@ -1,4 +1,4 @@
-using Znak.Model.Entities;
+﻿using Znak.Model.Entities;
 
 namespace Znak.Model.MockData
 {
@@ -6,86 +6,149 @@ namespace Znak.Model.MockData
     {
         public static void CreateMockData(this EFContext Context)
         {
-            var userSanjar = new User()
+            // Создаем 20 пользователей
+            var userNames = new List<string> { "Александр", "Иван", "Мария", "Елена", "Андрей", "Сергей", "Ольга", "Наталья", "Дмитрий", "Евгений", "Анна", "Екатерина", "Михаил", "Николай", "Владимир", "Татьяна", "Алексей", "Виктория", "Ирина", "Павел" };
+            var userLastNames = new List<string> { "Иванов", "Петров", "Сидоров", "Смирнов", "Кузнецов", "Попов", "Васильев", "Павлов", "Семенов", "Голубев", "Виноградов", "Богданов", "Воробьев", "Федоров", "Михайлов", "Беляев", "Тарасов", "Белов", "Козлов", "Лебедев" };
+
+            for (int i = 0; i < userNames.Count; i++)
             {
-                FirstName = "Sanjar",
-                LastName = "Akhmedov",
-                Email = "sanjar@gmail.com",
-                Phone = "927470533",
-                Address = "Khujand",
-                UserRole = UserRole.Admin,
-                Login = "sanjar",
-                Password = "sanjar",
-                Avatar = null
+                var user = new User()
+                {
+                    FirstName = userNames[i],
+                    LastName = userLastNames[i],
+                    Email = $"user{i + 1}@example.com",
+                    Phone = $"+7 (999) 123-45-6{i + 1}",
+                    Address = $"Улица {i + 1}, дом {i + 1}",
+                    UserRole = UserRole.Admin,
+                    Login = $"user{i + 1}",
+                    Password = $"password{i + 1}",
+                    Avatar = null // Добавьте ваш код для хранения аватаров
+                };
+                Context.Users.Add(user);
+            }
+
+            // Создаем 10 категорий продуктов
+            var categoryNames = new List<string> { "Брюки", "Футболки", "Платья", "Джинсы", "Рубашки", "Юбки", "Куртки", "Пальто", "Свитера", "Пиджаки" };
+            var categoryDescriptions = new List<string>
+            {
+                "Категория товаров - брюки.",
+                "Категория товаров - футболки.",
+                "Категория товаров - платья.",
+                "Категория товаров - джинсы.",
+                "Категория товаров - рубашки.",
+                "Категория товаров - юбки.",
+                "Категория товаров - куртки.",
+                "Категория товаров - пальто.",
+                "Категория товаров - свитера.",
+                "Категория товаров - пиджаки."
             };
 
-            Context.Users.Add(userSanjar);
+            var categories = new List<ProductCategory>();
 
-            var ProductCategory1 = new ProductCategory()
+            for (int i = 0; i < categoryNames.Count; i++)
             {
-                Name = "FirstProductCategory",
-                Description = "Very delicios"
-            };
-            var ProductCategory2 = new ProductCategory()
-            {
-                Name = "SecondProductCategory",
-                Description = "Very delicios"
-            };
-            var ProductCategory3 = new ProductCategory()
-            {
-                Name = "ThirdProductCategory",
-                Description = "For drinking"
-            };
-            Context.ProductCategories.Add(ProductCategory1);
-            Context.ProductCategories.Add(ProductCategory2);
-            Context.ProductCategories.Add(ProductCategory3);
+                var productCategory = new ProductCategory()
+                {
+                    Name = categoryNames[i],
+                    Description = categoryDescriptions[i]
+                };
+                categories.Add(productCategory);
+                Context.ProductCategories.Add(productCategory);
+            }
 
-            var unitMeasure = new UnitMeasure()
+            // Создаем 8 единиц измерения
+            var unitMeasureNames = new List<string> { "Штука", "Пара", "Грамм", "Килограмм", "Метр", "Квадратный метр", "Кубический метр", "Сантиметр" };
+            var unitMeasureDescriptions = new List<string>
             {
-                Name = "One",
-                Description = "for one unit"
+                "Единица измерения - штука.",
+                "Единица измерения - пара.",
+                "Единица измерения - грамм.",
+                "Единица измерения - килограмм.",
+                "Единица измерения - метр.",
+                "Единица измерения - квадратный метр.",
+                "Единица измерения - кубический метр.",
+                "Единица измерения - сантиметр."
             };
 
-            var unitMeasure1 = new UnitMeasure()
+            var unitMeasures = new List<UnitMeasure>();
+            for (int i = 0; i < unitMeasureNames.Count; i++)
             {
-                Name = "kg",
-                Description = "for kg"
-            };
-            var unitMeasure2 = new UnitMeasure()
-            {
-                Name = "1/2 unit",
-                Description = "for 1/2"
-            };
-            var unitMeasure3 = new UnitMeasure()
-            {
-                Name = "1/4 unit",
-                Description = "for 1/4"
-            };
-            Context.UnitMeasures.Add(unitMeasure);
-            Context.UnitMeasures.Add(unitMeasure1);
-            Context.UnitMeasures.Add(unitMeasure2);
-            Context.UnitMeasures.Add(unitMeasure3);
+                var unitMeasure = new UnitMeasure()
+                {
+                    Name = unitMeasureNames[i],
+                    Description = unitMeasureDescriptions[i]
+                };
+                unitMeasures.Add(unitMeasure);
+                Context.UnitMeasures.Add(unitMeasure);
+            }
 
-            var Product1 = new Product()
+            // Создаем 20 продуктов
+            var productNames = new List<string>
             {
-                Name = "Osh",
-                Description = "National Product. Very good",
-                Price = 14,
-                ProductCategory = ProductCategory1,
-                UnitMeasure = unitMeasure,
+                "Брюки спортивные",
+                "Футболка хлопковая",
+                "Платье вечернее",
+                "Джинсы классические",
+                "Рубашка с коротким рукавом",
+                "Юбка мини-длины",
+                "Куртка кожаная",
+                "Пальто демисезонное",
+                "Свитер с воротником",
+                "Пиджак офисный",
+                "Джемпер вязаный",
+                "Шорты для плавания",
+                "Блузка шифоновая",
+                "Пиджак из шерсти",
+                "Костюм спортивный",
+                "Жакет классический",
+                "Шарф шерстяной",
+                "Платье повседневное",
+                "Костюм тематический",
+                "Шляпа летняя"
             };
-            var Product2 = new Product()
+
+            var productDescriptions = new List<string>
             {
-                Name = "Kebab",
-                Description = "National Product. Very good",
-                Price = 18,
-                ProductCategory = ProductCategory1,
-                UnitMeasure = unitMeasure,
+                "Спортивные брюки для активного отдыха и занятий спортом.",
+                "Хлопковая футболка с круглым вырезом и короткими рукавами.",
+                "Вечернее платье из ткани с блестящим эффектом и глубоким декольте.",
+                "Классические джинсы прямого кроя из денима с натуральным составом.",
+                "Рубашка с короткими рукавами из легкого материала с принтом.",
+                "Юбка мини-длины из эластичного материала с застежкой на молнию.",
+                "Кожаная куртка с воротником-стойкой и двумя боковыми карманами.",
+                "Демисезонное пальто с отложным воротником и поясом на талии.",
+                "Свитер с воротником-стойкой из мягкого материала с крупной вязкой.",
+                "Офисный пиджак прямого кроя из хлопковой ткани с двумя пуговицами.",
+                "Джемпер вязаный с капюшоном и карманами из теплой пряжи.",
+                "Шорты для плавания из быстросохнущего материала с подкладкой.",
+                "Блузка шифоновая с V-образным вырезом и красивым принтом.",
+                "Пиджак из шерстяной ткани с двубортной застежкой и классическим кроем.",
+                "Спортивный костюм из трикотажа с капюшоном и брюками на резинке.",
+                "Жакет классический с английским воротником и двумя карманами.",
+                "Шерстяной шарф с крупной вязкой и косыми краями.",
+                "Повседневное платье из хлопковой ткани с рукавами три четверти.",
+                "Тематический костюм для праздничного мероприятия с ярким принтом.",
+                "Летняя шляпа из натуральной соломы с широкими полями."
             };
-            Context.Products.Add(Product1);
-            Context.Products.Add(Product2);
+
+            var categoryIds = categories.Select(c => c.Id).ToList();
+            var unitIds = unitMeasures.Select(u => u.Id).ToList();
+
+            for (int i = 0; i < 20; i++)
+            {
+                var product = new Product()
+                {
+                    Name = productNames[i],
+                    Description = productDescriptions[i],
+                    Price = (i + 1) * 24,
+                    ProductCategory = categories.FirstOrDefault(p => p.Id == categoryIds[i % categoryIds.Count]),
+                    UnitMeasure = unitMeasures.FirstOrDefault(u => u.Id == unitIds[i % unitIds.Count])
+                };
+                Context.Products.Add(product);
+            }
 
             Context.SaveChanges();
+
         }
     }
 }
